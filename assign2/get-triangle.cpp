f43240f210124330f7d14d8728862089d7bcdf52
enlighter <mandal.sushovan92@gmail.com>
2016-02-05 03:37:15 +0530

    input and store input strings

--- assign2/get-triangle.cpp
+++ assign2/get-triangle.cpp
@@ -7,13 +7,47 @@
 
 
 #include <iostream>
+#include <string>
+#include <regex>
 
 #include "Polygon2d.cpp"
+
+#define COORDINATE_SYSTEM	3 //3d coordinate system used here
+#define POLYGON_SIZE	3
 
 using namespace std;
 
 int main()
 {
+	string points[POLYGON_SIZE];
+	int vertices[POLYGON_SIZE][COORDINATE_SYSTEM];
+	int i=0, j=0;
+
+	try
+	{
+		cout<<"Enter the coordinates for the 3 vertices of the 3D triangle:"<<endl;
+
+		for(i=0; i<POLYGON_SIZE; i++)
+		{
+			cout<<"Enter vertex no. "<<i<<" coordinates in the format (x,y,z): ";
+			getline(cin, points[i]);
+		}
+		
+		for(i=0; i<POLYGON_SIZE; i++)
+		{
+			cout<<points[i]<<endl;
+		}
+		//process_coordinates(start, start_point);
+		//process_coordinates(end, end_point);
+
+		//draw3Dline(start_point, end_point);
+	}
+	catch(const char* msg)
+	{
+		cerr<<msg<<endl;
+		return 0;
+	}
+
 	return 1;
 }
 

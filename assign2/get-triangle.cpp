275615bfb3723f2f8a00ffe8d262604ee80cc058
enlighter <mandal.sushovan92@gmail.com>
2016-02-05 04:04:28 +0530

    process input string and extract the coordinate system

--- assign2/get-triangle.cpp
+++ assign2/get-triangle.cpp
@@ -14,8 +14,99 @@
 
 #define COORDINATE_SYSTEM	3 //3d coordinate system used here
 #define POLYGON_SIZE	3
+ #define FORMAT_ERROR "Please maintain proper format!"
 
 using namespace std;
+
+int process_coordinates(string input, int point[])
+{
+	std::string::iterator itf = input.begin();
+	//int point[3];
+	//int flag=2;
+
+	/* check that the string is not empty
+		and it starts with a '(' and ends with a ')'*/
+	if (input.empty())
+		throw "No value given";
+	else if (*itf != '(')
+		throw FORMAT_ERROR;
+
+	itf = input.end();
+	itf--;
+	if (*itf != ')')
+		throw FORMAT_ERROR;
+	/*---------------------------------------*/
+
+	input = input.substr(1, input.length()-2);
+	//DEBUG
+	//cout<<input<<endl;
+
+	try
+	{
+		if (regex_match (input, regex("[[:digit:]]+\\,[[:digit:]]+\\,[[:digit:]]+") ))
+			// regex comparison expression using ECMAScript syntax
+		{
+			cout<<"regex match!";//<<endl;
+
+			//Split the regex complaint string along ',' character
+			stringstream temp(input);
+			string segment;
+			vector<string> seglist;
+			while(getline(temp, segment, ','))
+			{
+   				seglist.push_back(segment);
+			}
+			int j=0;
+			for (vector<string>::const_iterator i = seglist.begin(); i != seglist.end(); ++i, j++)
+			{
+				//DEBUG
+    			//cout << *i << ' ';
+    			point[j] = std::stoi(*i, NULL, 10); //convert to integer value
+			}
+    		cout<<endl;
+
+    		//DEBUG
+    		/*for(j=0; j<COORDINATE_SYSTEM; j++)
+    			cout<<point[j]<<" ";
+			*/
+			
+    		return 1;
+		}
+		else
+			throw FORMAT_ERROR;
+	}
+	catch(regex_error& e)
+	{
+		//extensive regex_error handling
+		if (e.code() == regex_constants::error_collate)
+			throw "The expression contained an invalid collating element name.";
+		//else if (e.code() == regex_constants::codeerror_ctype)
+			//throw "The expression contained an invalid character class name.";
+		else if (e.code() == regex_constants::error_escape)
+			throw "The expression contained an invalid escaped character, or a trailing escape.";
+		else if (e.code() == regex_constants::error_backref)
+			throw "The expression contained an invalid back reference.";
+		else if (e.code() == regex_constants::error_brack)
+			throw "The expression contained mismatched brackets ([ and ]).";
+		else if (e.code() == regex_constants::error_paren)
+			throw "The expression contained mismatched parentheses (( and )).";
+		else if (e.code() == regex_constants::error_brace)
+			throw "The expression contained mismatched braces ({ and }).";
+		else if (e.code() == regex_constants::error_badbrace)
+			throw "The expression contained an invalid range between braces ({ and }).";
+		else if (e.code() == regex_constants::error_range)
+			throw "The expression contained an invalid character range.";
+		else if (e.code() == regex_constants::error_space)
+			throw "There was insufficient memory to convert the expression into a finite state machine.";
+		else if (e.code() == regex_constants::error_badrepeat)
+			throw "The expression contained a repeat specifier (one of *?+{) that was not preceded by a valid regular expression.";
+		else if (e.code() == regex_constants::error_complexity)
+			throw "The complexity of an attempted match against a regular expression exceeded a pre-set level.";
+		else if (e.code() == regex_constants::error_stack)
+			throw "There was insufficient memory to determine whether the regular expression could match the specified character sequence.";
+	}
+
+}
 
 int main()
 {
@@ -35,9 +126,18 @@ int main()
 		
 		for(i=0; i<POLYGON_SIZE; i++)
 		{
-			cout<<points[i]<<endl;
+			//cout<<points[i]<<endl;
+			process_coordinates(points[i], vertices[i]);
+			cout<<"Received point : ";
+			for(j=0; j<COORDINATE_SYSTEM; j++)
+			{
+				cout<<vertices[i][j];
+				if(j<COORDINATE_SYSTEM-1)
+					cout<<",";
+			}
+			cout<<endl;
 		}
-		//process_coordinates(start, start_point);
+		
 		//process_coordinates(end, end_point);
 
 		//draw3Dline(start_point, end_point);

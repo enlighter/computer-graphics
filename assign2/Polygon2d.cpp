09845a88e0f62b1a644e9a59cdd5033f1018c6a9
enlighter <mandal.sushovan92@gmail.com>
2016-02-04 06:21:51 +0530

    give initial description to the 2d polygon class

--- assign2/Polygon2d.cpp
+++ assign2/Polygon2d.cpp
@@ -5,11 +5,36 @@
  *      Author: enlighter
  */
 
+#include <stdexcept>
+#include <algorithm> //std::sort
+#include <stdio.h> //printf
+
 #include "Polygon2d.h"
 
-Polygon2d::Polygon2d() {
-	// TODO Auto-generated constructor stub
+using namespace std;
 
+Polygon2d::Polygon2d(std::vector<vertex> vertices,int size)
+//takes the polygon vertices and size as parameter input
+{
+	// check that sufficient no. of vertices has been provided
+	if(vertices.size() != size)
+	{
+		throw std::invalid_argument( "proper no. of vertices not provided" );
+	}
+
+	//sort the vertices, default is sorting by 'y' asc
+	std::sort(vertices.begin(), vertices.end());
+
+	for(std::vector<vertex>::iterator it=vertices.begin(); it != vertices.end(); ++it)
+	{
+		printf("x: %d, y: %d\n", *it->x, *it->y);
+
+		eth temp;
+		temp.y = *it->y;
+		temp.l = NULL;
+
+		edge_table.push_back(temp);
+	}
 }
 
 Polygon2d::~Polygon2d() {
--- assign2/Polygon2d.h
+++ assign2/Polygon2d.h
@@ -4,13 +4,44 @@
  *  Created on: 02-Feb-2016
  *      Author: enlighter
  */
+#include <iostream>
+#include <list>
+#include <vector>
 
 #ifndef POLYGON2D_H_
 #define POLYGON2D_H_
 
+//#define COORDINATE_SYSTEM	2
+
+using namespace std;
+
 class Polygon2d {
+private:
+	class vertex
+	{
+	public:
+		int x,y;
+		//overload '<' operator for sorting using std::sort
+		bool operator<(const vertex &rhs) const { return y < rhs.y; }
+	};
+
+	typedef struct edge_table_list_member
+	{
+		int x; //x coordinate
+		int delta_x, delta_y;
+	} etlm;
+
+	typedef struct edge_table_header
+	{
+		int y;
+		std::list<etlm> *l;
+	} eth;
+
+	//define the global edge table
+	std::vector<eth> edge_table;
+
 public:
-	Polygon2d();
+	Polygon2d(std::vector<vertex>, int);
 	virtual ~Polygon2d();
 };
 
